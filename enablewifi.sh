633cc0f1e8ed62d894e5d7fff1f6a2395f3ac756
wuli <wuli7388668@gmail.com>
2024-12-24 11:22:04 +0800

    fixed

--- enablewifi.sh
+++ enablewifi.sh
@@ -4,13 +4,15 @@
 # 检查是否为Deepin系统
 if grep -q "deepin" /etc/os-release; then
     echo "当前系统是Deepin"
-    
-    # 修改 /etc/modprobe.d/blacklist-bcm43.conf 文件
-    sudo sed -i 's/^#blacklist brcm80211/blacklist brcm80211/' /etc/modprobe.d/blacklist-bcm43.conf
-    sudo sed -i 's/^#blacklist brcmfmac/blacklist brcmfmac/' /etc/modprobe.d/blacklist-bcm43.conf
-    
-    # 将 brcmfmac 添加到 /etc/modules 文件
-#    echo "brcmfmac" | sudo tee -a /etc/modules
+     # 修改 /etc/modprobe.d/blacklist-bcm43.conf，注释以下两行
+    sudo sed -i '/blacklist brcm80211/ s/^/#/' /etc/modprobe.d/blacklist-bcm43.conf
+    sudo sed -i '/blacklist brcmfmac/ s/^/#/' /etc/modprobe.d/blacklist-bcm43.conf
+ 
+    # 将 brcmfmac 添加到 /etc/modules
+    echo "brcmfmac" | sudo tee -a /etc/modules
+ 
+    echo "操作完成！"
+
 else
     echo "当前系统不是Deepin"
 fi
